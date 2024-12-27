8e358298d5e7770dc657356de8b52db6d5eee065
Asiman <asiman.ismayilova.2006@mail.ru>
2024-12-27 22:24:02 +0400

    feat: add ToDoRepo interface extending JpaRepository

---
+++ ToDo-App/src/main/java/com/example/ToDo_App/repository/ToDoRepo.java
@@ -0,0 +1,11 @@
+package com.example.ToDo_App.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.ToDo_App.model.ToDo;
+
+@Repository
+public interface ToDoRepo extends JpaRepository <ToDo, Long> {
+    
+}
